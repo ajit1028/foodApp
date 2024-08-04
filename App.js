8462d86aa931a7654d7bf0aeb9902b772d6ff6c9
ajit <ajeets@1028.com>
2024-08-04 16:57:42 +0530

    updates

--- App.js
+++ App.js
@@ -1,29 +1,16 @@
-/*
- * <div id ="parent">
-           <div id="child">
-             <h1> i am  h1 tag  <h1>
-            <h1> i am  h1 tag  <h1>
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-           </div>
-
-             <div id="child">
-             <h1> i am  h1 tag  <h1>
-            <h1> i am  h1 tag  <h1>
-
-           </div>
- * </div> 
- * 
- */
 const parent = React.createElement("div", { id: "parent" }, [
   [
-    React.createElement("h1", { id: "child" }, "i am h1 tag"),
+    React.createElement("h1", { id: "child" }, "This is Namaste Raect"),
     (React.createElement("h1", { id: "child" }, "i am h1 tag"),
     React.createElement("h2", { id: "child" }, "i am h2 tag")),
   ],
   [
-    React.createElement("h1", { id: "child" }, "i am h1 tag"),
+    React.createElement("h1", { id: "child" }, "i am h1 Hello"),
     (React.createElement("h1", { id: "child" }, "i am h1 tag"),
-    React.createElement("h2", { id: "child" }, "i am h2 tag")),
+    React.createElement("h2", { id: "child" }, "i am h2 Hello")),
   ],
 ]);
 
