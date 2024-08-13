973717c7be437898b24f5c2e8f646d911e353126
ajit <ajeets@1028.com>
2024-08-13 18:18:17 +0530

    tailwindCss applied

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -35,21 +35,23 @@ const Body = () => {
       <h1>It looks like you are offline !!!.Check your internet Connection.</h1>
     );
 
-  return listOfResturants.length === 0 ? (
+  return listOfResturants === undefined || listOfResturants.length === 0 ? (
     <Shimmer />
   ) : (
     <div className="body">
-      <div className="filter ">
-        <div className="search">
+      <div className="filter flex">
+        <div className="search p-4 m-4">
           <input
             type="text"
-            className="search-box"
+            className="border border-solid border-black"
             value={searchText}
             onChange={(e) => {
               setSearchText(e.target.value);
             }}
           />
+
           <button
+            className="px-4 py-2 bg-green-100 m-4 rounded-lg"
             onClick={() => {
               console.log(searchText);
               const crntFilteredList = listOfResturants.filter((res) =>
@@ -62,19 +64,22 @@ const Body = () => {
             Search
           </button>
         </div>
-        <button
-          className="filter-btn"
-          onClick={() => {
-            const filteredList = listOfResturants.filter(
-              (res) => res.info.avgRating > 4.5
-            );
-            setListOfResturant(filteredList);
-          }}
-        >
-          Top Rated Resturants{" "}
-        </button>
+
+        <div className="search p-4 m-4 flex items-center ">
+          <button
+            className="px-4 py-2 bg-gray-100 rounded-lg"
+            onClick={() => {
+              const filteredList = listOfResturants.filter(
+                (res) => res.info.avgRating > 4
+              );
+              setListOfResturant(filteredList);
+            }}
+          >
+            Top Rated Resturants{" "}
+          </button>
+        </div>
       </div>
-      <div className="res-container">
+      <div className="flex flex-wrap">
         {filteredResturant.map((resturant) => (
           <Link key={resturant.info.id} to={"/resturants/" + resturant.info.id}>
             <ResturantCard resData={resturant} />
